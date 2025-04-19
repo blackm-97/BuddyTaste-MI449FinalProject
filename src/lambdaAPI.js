d4ce92b22e457591171113b6dcac6a04b33a910c
blackm-97 <blackm97@msu.edu>
2025-04-19 01:04:51 +0000

    Can send and recieve data from server

--- src/lambdaAPI.js
+++ src/lambdaAPI.js
@@ -4,7 +4,13 @@ export async function getData() {
     const url = "https://f4ckavlbp6.execute-api.us-east-2.amazonaws.com/movies";
     let message = 'Failed'
     try {
-      const response = await fetch(url);
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ movie_name: 'Inception' })
+      });
       if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
       }
